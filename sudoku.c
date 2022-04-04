324be381cbaa80726b1a893f07e7a40bc7975be5
SeaOfMares <maresdavide6704@outlook.com>
2022-04-04 13:21:37 -0500

    Adding write to file inside sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 extern int trycell(int*, int);
+
+void write_file(char*str, char* file_name);
+// Write 32-bit int into mem file
+void write_file(char* str, char* file_name){
+	FILE* mem_file;
+	FILE* temp_file;
+ 	// open file with relative address
+	mem_file = fopen(file_name, "r");
+	temp_file = fopen("replace.tmp", "w");
+
+	/* fopen() return NULL if unable to open file in given mode. */
+   	 if (mem_file == NULL || temp_file == NULL){
+		/* Unable to open file hence exit */
+		printf("\nUnable to open file.\n");
+		printf("Please check whether file exists and you have read/write privilege.\n");
+		return -1;
+    	}
+
+	fputs(str,temp_file);
+    	// release resources back to OS
+    	fclose(mem_file);
+    	fclose(temp_file);
+
+	rename("replace.tmp", file_name);
+
+}
 
 void show(int *x)
 {
